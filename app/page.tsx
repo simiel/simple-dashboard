71cd6185a2393c7eaaae862c2af9ee86c331474d
Simiel <samensah036@st.ug.edu.gh>
2023-12-26 09:02:46 +0000

    started layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
 
 import styles from '@/app/ui/home.module.css';
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -32,6 +33,20 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src={'/hero-desktop.png'}
+            width={1000}
+            height={720}
+            className="hidden md:block"
+            alt="Dashboard with graphs and charts"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshot of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
